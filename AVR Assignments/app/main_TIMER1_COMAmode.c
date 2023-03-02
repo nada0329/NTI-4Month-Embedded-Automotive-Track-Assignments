9284df9a498f3256ac4be306287383bd2cbfbeba
unknown <nada66811@gmail.com>
2023-03-02 15:54:29 +0200

    use TIMER1 CTC mode INTA to generate event each 2sec

---
+++ AVR Assignments/app/main_TIMER1_COMAmode.c
@@ -0,0 +1,51 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 2/24/2023 9:34:22 AM
+ * Author : Nada
+ */ 
+
+
+#include "LED.h"
+#include "DIO.h"
+#include "kit_cnfg.h"
+#include "TIMR1_int.h"
+#include "TIMR1_priv.h"
+#include "TIMR1_config.h"
+#include "GlobalINT.h"
+
+
+
+void func_c1(void);
+
+int main(void)
+{
+	LED_init(LED_RED_PORT,LED_RED_PIN);
+	Timer1_SetCallBack_OCMA(func_c1);
+	Timer1_Init();
+	Timer1_SetTimingLimits_cmpA(50000);
+	Timer1_OCMAInt_Enable();
+	GlobalINT_Enable();
+
+	while (1)
+	{
+		
+		
+	}
+}
+
+void func_c1(void)
+{
+	static volatile u8 count1_t=0;
+	if(count1_t==39)
+	{
+		count1_t=0;
+		LED_TOG(LED_RED_PORT,LED_RED_PIN);
+	}
+	else
+	{
+		count1_t++;
+	}
+	
+	
+}
